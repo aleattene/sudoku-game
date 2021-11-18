7dca048a621c0fbd8588eafd7fac5ae9a7709baa
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-18 12:21:27 +0100

    feat: completed first POC with the generation of the sudoku board (9 x 9)

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def check_mini_sudoku(matrix):
 # print(check_mini_sudoku([[0, 1, 2], [6, 4, 5], [9, 8, 7]]))  # False - The 0 is included (outside range)
 # print(check_mini_sudoku([[8, 9, 2], [5, 6, 1], [3, 7, 4]]))  # True
 
-sudoku = []
-
 
 def generate_mini_sudoku(n):
     numbers = [x for x in range(1, 10)]
@@ -40,7 +38,6 @@ def generate_mini_sudoku_right(mini_sudoku):
 
 
 def generate_mini_sudoku_down(mini_sudoku):
-    print(mini_sudoku)
     mini_sudoku_down = []
     mini_sudoku_down.append(mini_sudoku[0][2:] + mini_sudoku[0][0:1] + mini_sudoku[0][1:2])
     mini_sudoku_down.append(mini_sudoku[1][2:] + mini_sudoku[1][0:1] + mini_sudoku[1][1:2])
@@ -48,25 +45,36 @@ def generate_mini_sudoku_down(mini_sudoku):
     return mini_sudoku_down
 
 
-# 1
-sudoku_one = generate_mini_sudoku(3)
-sudoku.append(sudoku_one)
-print(sudoku_one)
-# 2
-sudoku_two = generate_mini_sudoku_right(sudoku_one)
-sudoku.append(sudoku_two)
-# 3
-sudoku_three = generate_mini_sudoku_right(sudoku_two)
-sudoku.append(sudoku_three)
-# 4
-sudoku_four = generate_mini_sudoku_down(sudoku_one)
-sudoku.append(sudoku_four)
-# 5
-sudoku_five = generate_mini_sudoku_right(sudoku_four)
-sudoku.append(sudoku_five)
-# 6
-sudoku_six = generate_mini_sudoku_right(sudoku_five)
-sudoku.append(sudoku_six)
-# 7
-sudoku_four = generate_mini_sudoku_down(sudoku_one)
-sudoku.append(sudoku_four)
+for i in range(1, 4):
+    print(f"Board number {i}")
+    sudoku = []
+    # 1
+    sudoku_one = generate_mini_sudoku(3)
+    sudoku.append(sudoku_one)
+    # 2
+    sudoku_two = generate_mini_sudoku_right(sudoku_one)
+    sudoku.append(sudoku_two)
+    # 3
+    sudoku_three = generate_mini_sudoku_right(sudoku_two)
+    sudoku.append(sudoku_three)
+    # 4
+    sudoku_four = generate_mini_sudoku_down(sudoku_one)
+    sudoku.append(sudoku_four)
+    # 5
+    sudoku_five = generate_mini_sudoku_right(sudoku_four)
+    sudoku.append(sudoku_five)
+    # 6
+    sudoku_six = generate_mini_sudoku_right(sudoku_five)
+    sudoku.append(sudoku_six)
+    # 7
+    sudoku_seven = generate_mini_sudoku_down(sudoku_four)
+    sudoku.append(sudoku_seven)
+    # 8
+    sudoku_eight = generate_mini_sudoku_right(sudoku_seven)
+    sudoku.append(sudoku_eight)
+    # 9
+    sudoku_nine = generate_mini_sudoku_right(sudoku_eight)
+    sudoku.append(sudoku_nine)
+
+    for row in sudoku:
+        print(row)
