2fd5ab20323124319db237b04a0ca3e087f9b153
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-18 10:56:28 +0100

    feat: added a function that generate a mini sudoku (a 3x3 square) containing the numbers 1 to 9 exactly once.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Create a function that checks to make sure this 3x3 square contains each number 
 Make sure there are no duplicates, and no numbers outside this range
 """
 
+import random
+
 
 def check_mini_sudoku(matrix):
     return True if "123456789" == "".join(str(item) for item in sorted(matrix[0] + matrix[1] + matrix[2])) else False
@@ -16,3 +18,15 @@ print(check_mini_sudoku([[1, 3, 2], [9, 7, 8], [4, 5, 6]]))  # True
 print(check_mini_sudoku([[1, 1, 3], [6, 5, 4], [8, 7, 9]]))  # False - The 1 is repeated twice
 print(check_mini_sudoku([[0, 1, 2], [6, 4, 5], [9, 8, 7]]))  # False - The 0 is included (outside range)
 print(check_mini_sudoku([[8, 9, 2], [5, 6, 1], [3, 7, 4]]))  # True
+
+sudoku = []
+
+
+def generate_mini_sudoku(n):
+    numbers = [x for x in range(1, 10)]
+    mini_sudoku = []
+    for num in range(n):
+        i = [x for x in random.sample(numbers, n)]
+        print(i)
+        mini_sudoku.append(i)
+    return mini_sudoku
