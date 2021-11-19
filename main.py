45c562344d1b9f67e4f8b09a59e56774bbac320f
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-19 12:57:49 +0100

    feat: added the correctness check of each mini sudoku generated

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def check_mini_sudoku(matrix):
 def generate_mini_sudoku(n):
     numbers = [x for x in range(1, 10)]
     mini_sudoku = []
-    for i in range(n):
-        row = []
+    for _ in range(n):
+        line = []
         for j in range(n):
             number = random.choice(numbers)
-            row.append(number)
+            line.append(number)
             numbers.remove(number)
-        mini_sudoku.append(row)
+        mini_sudoku.append(line)
     return mini_sudoku
 
 
@@ -38,43 +38,52 @@ def generate_mini_sudoku_right(mini_sudoku):
 
 
 def generate_mini_sudoku_down(mini_sudoku):
-    mini_sudoku_down = []
-    mini_sudoku_down.append(mini_sudoku[0][2:] + mini_sudoku[0][0:1] + mini_sudoku[0][1:2])
-    mini_sudoku_down.append(mini_sudoku[1][2:] + mini_sudoku[1][0:1] + mini_sudoku[1][1:2])
-    mini_sudoku_down.append(mini_sudoku[2][2:] + mini_sudoku[2][0:1] + mini_sudoku[2][1:2])
+    mini_sudoku_down = [mini_sudoku[0][2:] + mini_sudoku[0][0:1] + mini_sudoku[0][1:2],
+                        mini_sudoku[1][2:] + mini_sudoku[1][0:1] + mini_sudoku[1][1:2],
+                        mini_sudoku[2][2:] + mini_sudoku[2][0:1] + mini_sudoku[2][1:2]]
     return mini_sudoku_down
+
 
 
 for i in range(1, 4):
     print(f"Board number {i}")
     sudoku = []
-    # 1
+    # MINI SUDOKU 01
     sudoku_one = generate_mini_sudoku(3)
-    sudoku.append(sudoku_one)
-    # 2
+    if check_mini_sudoku(sudoku_one):
+        sudoku.append(sudoku_one)
+    # MINI SUDOKU 02
     sudoku_two = generate_mini_sudoku_right(sudoku_one)
-    sudoku.append(sudoku_two)
-    # 3
+    if check_mini_sudoku(sudoku_two):
+        sudoku.append(sudoku_two)
+    # MINI SUDOKU 03
     sudoku_three = generate_mini_sudoku_right(sudoku_two)
-    sudoku.append(sudoku_three)
-    # 4
+    if check_mini_sudoku(sudoku_three):
+        sudoku.append(sudoku_three)
+    # MINI SUDOKU 04
     sudoku_four = generate_mini_sudoku_down(sudoku_one)
-    sudoku.append(sudoku_four)
-    # 5
+    if check_mini_sudoku(sudoku_four):
+        sudoku.append(sudoku_four)
+    # MINI SUDOKU 05
     sudoku_five = generate_mini_sudoku_right(sudoku_four)
-    sudoku.append(sudoku_five)
-    # 6
+    if check_mini_sudoku(sudoku_five):
+        sudoku.append(sudoku_five)
+    # MINI SUDOKU 06
     sudoku_six = generate_mini_sudoku_right(sudoku_five)
-    sudoku.append(sudoku_six)
-    # 7
+    if check_mini_sudoku(sudoku_six):
+        sudoku.append(sudoku_six)
+    # MINI SUDOKU 07
     sudoku_seven = generate_mini_sudoku_down(sudoku_four)
-    sudoku.append(sudoku_seven)
-    # 8
+    if check_mini_sudoku(sudoku_seven):
+        sudoku.append(sudoku_seven)
+    # MINI SUDOKU 08
     sudoku_eight = generate_mini_sudoku_right(sudoku_seven)
-    sudoku.append(sudoku_eight)
-    # 9
+    if check_mini_sudoku(sudoku_eight):
+        sudoku.append(sudoku_eight)
+    # MINI SUDOKU 09
     sudoku_nine = generate_mini_sudoku_right(sudoku_eight)
-    sudoku.append(sudoku_nine)
+    if check_mini_sudoku(sudoku_nine):
+        sudoku.append(sudoku_nine)
 
     for row in sudoku:
         print(row)
